48a5c3e33bc8389a9d1d1c4d4d93cb0437359ecd
Bibhu Dutta Pradhan <bibhu.pradhan@agilink.com>
2024-06-20 13:57:59 +0530

    commit

--- FizzBuzz.Services.Generator/Service/FizzBuzzGenerator.cs
+++ FizzBuzz.Services.Generator/Service/FizzBuzzGenerator.cs
@@ -17,30 +17,7 @@ namespace FizzBuzz.Services.Generator.Service
                     int inputNumber;
                     if (!string.IsNullOrEmpty(input) && int.TryParse(input, out inputNumber))
                     {
-                        // Check if i is divisible by both 3 and 5 
-                        if (inputNumber % 3 == 0 && inputNumber % 5 == 0)
-                        {
-                            // Add "FizzBuzz" to the result list 
-                            result.Add(Constants.FizzBuzz);
-                        }
-                        // Check if i is divisible by 3 
-                        else if (inputNumber % 3 == 0)
-                        {
-                            // Add "Fizz" to the result list 
-                            result.Add(Constants.Fizz);
-                        }
-                        // Check if i is divisible by 5 
-                        else if (inputNumber % 5 == 0)
-                        {
-                            // Add "Buzz" to the result list 
-                            result.Add(Constants.Buzz);
-                        }
-                        else
-                        {
-                            // Add "Buzz" to the result list
-                            string temp = $"Divided {input} by 3, Divided {input} by 5";
-                            result.Add(temp);
-                        }
+                        Utility.Common.CheckDivisibilty(inputNumber, out result);
                     }
                     else
                     {
---
+++ FizzBuzz.Services.Generator/Utility/Common.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace FizzBuzz.Services.Generator.Utility
+{
+    public static class Common
+    {
+        public static void CheckDivisibilty(int dividend, out List<string> result)
+        {
+            List<string> tempresult = new List<string>();
+
+            // Check if i is divisible by both 3 and 5 
+            if (dividend % 3 == 0 && dividend % 5 == 0)
+            {
+                // Add "FizzBuzz" to the result list 
+                tempresult.Add(Constants.FizzBuzz);
+            }
+            // Check if i is divisible by 3 
+            else if (dividend % 3 == 0)
+            {
+                // Add "Fizz" to the result list 
+                tempresult.Add(Constants.Fizz);
+            }
+            // Check if i is divisible by 5 
+            else if (dividend % 5 == 0)
+            {
+                // Add "Buzz" to the result list 
+                tempresult.Add(Constants.Buzz);
+            }
+            else
+            {
+                // Add "Buzz" to the result list
+                string temp = $"Divided {dividend} by 3, Divided {dividend} by 5";
+                tempresult.Add(temp);
+            }
+            result = tempresult; 
+        }
+    }
+}
